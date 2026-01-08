2a2111ec66a842c6b2677d3804219c1c6c30083f
Rithanya <rithanyamanoharan100@gmail.com>
2026-01-09 00:48:46 +0530

    post,put

--- src/main/java/com/example/first/Todo.java
+++ src/main/java/com/example/first/Todo.java
@@ -1,12 +1,30 @@
 package com.example.first;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/todo")
 public class Todo {
+    //Path variable
+    @GetMapping("{id}")
+    String todo(@PathVariable int id){
+        return "welcome"+id;
+    }
+    //RequestParam
     @GetMapping
-    String todo(){
-        return "welcome";
+    String todoParam(@RequestParam String username){
+        return username;
+    }
+    @PostMapping("/create")
+    String create(@RequestBody String body){
+        return body;
+    }
+    @PutMapping("{id}")
+    String todoupdate(@PathVariable int id){
+        return "welcome"+id;
+    }
+    @DeleteMapping("{id}")
+    String tododelete(@PathVariable int id){
+        return "welcome"+id;
     }
 }
